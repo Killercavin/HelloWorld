731846ce7cb8828516edb606856508f10c47b039
Cavin <cavinlarry001@gmail.com>
2024-02-20 06:58:52 +0300

    Update involving deletion and update of existing files

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -10,10 +10,14 @@ import android.app.Activity;
 import android.util.Log;
 import android.content.Intent;
 import android.view.View;
-
+import android.net.Uri;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    String msg = "Android : ";
+    String msg = "Android: ";
 
     /** Called when the activity, service is first created. */
     @Override
@@ -71,10 +75,42 @@ public class MainActivity extends Activity {
         Log.d(msg, "The stopService() event");
     }
 
-    public void broadcastIntent(View view){
+    public void broadcastIntent(View view) {
         Intent intent = new Intent();
-        intent.setAction("com.example.CUSTOM_INTENT"); sendBroadcast(intent);
+        intent.setAction("com.example.CUSTOM_INTENT");
+        sendBroadcast(intent);
     }
 
+    public void onClickAddName(View view) {
+        // Add a new student record
+        ContentValues values = new ContentValues();
+        values.put(StudentsProvider.NAME, ((EditText) findViewById(R.id.editText2)).getText().toString());
+        values.put(StudentsProvider.GRADE, ((EditText) findViewById(R.id.editText3)).getText().toString());
 
+        // Use the correct CONTENT_URI from StudentsProvider
+        Uri uri = getContentResolver().insert(StudentsProvider.CONTENT_URI, values);
+
+        Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    /* public void onClickRetrieveStudents(View view) {
+        // Retrieve student records
+        // Use the correct CONTENT_URI from StudentsProvider
+        Uri students = StudentsProvider.CONTENT_URI;
+        Cursor c = getContentResolver().query(students, null, null, null, "name");
+
+        if (c != null) {
+            if (c.moveToFirst()) {
+                do {
+                    Toast.makeText(this,
+                            c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                                    ", " + c.getString(c.getColumnIndex(StudentsProvider.NAME)) +
+                                    ", " + c.getString(c.getColumnIndex(StudentsProvider.GRADE)),
+                            Toast.LENGTH_SHORT).show();
+                } while (c.moveToNext());
+            }
+            c.close(); // Close the cursor when done
+        }
+    } */
+    // I have a problem with fixing the Retrieve code
 }
